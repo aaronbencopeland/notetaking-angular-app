1034f3bd7e67994cca4700f21f738853bdd2beaa
freddking <chatter.opals_0h@icloud.com>
2023-05-01 09:16:04 -0400

    Add mediaQueryListener for menu on winddow resize

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarOptions } from 'src/constants';
+import { mediaQueryListener } from 'src/utils/MediaQueries';
 
 @Component({
     selector: 'navbar',
@@ -8,9 +9,16 @@ import { NavbarOptions } from 'src/constants';
 })
 
 export class NavbarComponent {
-    menuCollapsed = false;
+    menuCollapsed = true;
     menuOptionData = NavbarOptions;
 
+    // Will set menuCollapsed to true if screen width eclipses 800px
+    toggleMenuQueryListener = mediaQueryListener('(min-width: 800px)').subscribe((matches) => {
+        if(matches) {
+            this.menuCollapsed = true;
+        }
+    });
+    
     toggleMenuCollapse(): void {
         this.menuCollapsed = !this.menuCollapsed;
     }
