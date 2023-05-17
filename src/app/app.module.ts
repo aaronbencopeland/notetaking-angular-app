cd23297279f356cfd986e5a968b132185b11e4a7
“aaronbencopeland” <aaronbencopeland@gmail.com>
2023-05-17 12:55:29 -0400

    resolved all issues with the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/index';
 import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserAnimationsModule,
